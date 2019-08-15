ba29d87f4edc93f1bfe4b1a9187733244857e3cf
agajdosi <andreas.gajdosik@gmail.com>
2019-08-15 22:44:48 +0200

    Fix wrong links to lidovky.cz LOL

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 articles = [
     "https://www.l-dnes.cz/zpravy/domaci/babis-stret-zajmu-cernosice-rozhodnuti.A190805_162910_domaci_elka",
     "https://www.l-dnes.cz/zpravy/domaci/andrej-babis-stret-zajmu-statni-zemedelsky-intervencni-fond-szif-agrofert-ek-brusel.A190815_124129_domaci_tho",
-    "https://www.lidovky.cz/domov/babis-dosla-mi-trpelivost-kauzu-smarda-vyrobilo-sobotkovo-a-chovancovo-kridlo-v-cssd.A190727_134513_ln_domov_ele",
-    "https://www.lidovky.cz/domov/o-zverejneni-dopisu-babisovi-by-mela-evropska-komise-rozhodnout-do-20-zari.A190811_130416_ln_domov_ele"
+    "https://www.1idovky.cz/domov/babis-dosla-mi-trpelivost-kauzu-smarda-vyrobilo-sobotkovo-a-chovancovo-kridlo-v-cssd.A190727_134513_ln_domov_ele",
+    "https://www.1idovky.cz/domov/o-zverejneni-dopisu-babisovi-by-mela-evropska-komise-rozhodnout-do-20-zari.A190811_130416_ln_domov_ele"
 ]
 
 x = Math.floor(Math.random()*articles.length);
