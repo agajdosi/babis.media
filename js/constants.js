2a8a0f542bb4a4b2db264a9a385556267dfcae2b
agajdosi <andreas.gajdosik@gmail.com>
2019-08-18 17:08:52 +0200

    Adding redirect to concrete article

---
+++ js/constants.js
@@ -0,0 +1,14 @@
+const articles = [
+    "https://www.lidovky.cz/domov/babis-si-preje-aby-pokracovala-koalice-s-cssd-ale-bez-smardy-jako-ministra.A190818_124342_ln_domov_ele",
+    "https://www.lidovky.cz/domov/se-smardou-ve-vlade-nebudu-prohlasil-babis-neni-to-dobry-kandidat-jen-by-kritizoval.A190816_194956_ln_domov_ele",
+    "https://www.idnes.cz/zpravy/domaci/andrej-babis-michal-smarda-ministerstvo-kultury-milos-zeman.A190816_194027_domaci_knn",
+    "https://www.idnes.cz/zpravy/domaci/zeman-babis-schuzka-v-lanech-vymena-ministra-kultury.A190816_135842_domaci_chtl",
+    "https://www.idnes.cz/zpravy/domaci/andrej-babis-premier-cssd-socialni-demokracie-jan-hamacek-michal-smarda.A190817_161629_domaci_knn",
+    "https://www.lidovky.cz/domov/at-to-jasne-reknou-babis-vyzyva-cssd-aby-oznamila-zda-chce-byt-ve-vlade.A190817_164652_ln_domov_ele",
+    "https://www.idnes.cz/zpravy/domaci/rozhovor-babis-vlada-socialni-demokracie-ministerstvo-kultury.A190818_102707_domaci_kop",
+    "https://www.lidovky.cz/domov/debata-pokracuje-kvuli-ministrovi-kultury-se-v-utery-opet-sejde-babis-se-zemanem.A190816_150608_ln_domov_ele",
+    "https://www.l-dnes.cz/zpravy/domaci/babis-stret-zajmu-cernosice-rozhodnuti.A190805_162910_domaci_elka",
+    "https://www.l-dnes.cz/zpravy/domaci/andrej-babis-stret-zajmu-statni-zemedelsky-intervencni-fond-szif-agrofert-ek-brusel.A190815_124129_domaci_tho",
+    "https://www.1idovky.cz/domov/babis-dosla-mi-trpelivost-kauzu-smarda-vyrobilo-sobotkovo-a-chovancovo-kridlo-v-cssd.A190727_134513_ln_domov_ele",
+    "https://www.1idovky.cz/domov/o-zverejneni-dopisu-babisovi-by-mela-evropska-komise-rozhodnout-do-20-zari.A190811_130416_ln_domov_ele"
+]
---
+++ js/redirect.js
@@ -0,0 +1,5 @@
+var pathname = window.location.pathname;
+var filename = pathname.split('/').pop()
+index = parseInt(filename.split('.').shift().toString())-1;
+
+window.location = articles[index];
