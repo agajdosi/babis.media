0da024f1d258810e9b0ff9663cc35a8973ddd76d
agajdosi <andreas.gajdosik@gmail.com>
2019-09-15 21:02:37 +0200

    Random to newest articles, printed to ordered array

--- js/redirect.js
+++ js/redirect.js
@@ -2,4 +2,4 @@ var pathname = window.location.pathname;
 var filename = pathname.split('/').pop()
 index = parseInt(filename.split('.').shift().toString())-1;
 
-window.location = articles[index];
+window.location = printed[index];
